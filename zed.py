c5fa3072125d7bbf30657b195a7799201e63416e
John Saeger <john.saeger@gmail.com>
2018-03-01 02:24:07 -0800

    Add files via upload

--- zed.py
+++ zed.py
@@ -11,6 +11,7 @@
 # 2018-02-28 -- (1.02) update command descriptions
 #               fix repeat when n <= 0
 # 2018-02-28 -- (1.03) consistently
+# 2018-03-01 -- (1.04) modify change to use regular expressions
 
 """
 
@@ -22,8 +23,9 @@
  Commands
  --------
 
- nCs1/s2/    change first n occurences of s1 to s2 - position at start of
-             buffer - if n <= 0 change all
+ nCs1/s2/    change first n occurences of s1 to s2 after current position -
+             position doesn't change - you can use python flavored regular
+             expressions - if n <= 0 change all after current position
 
  nD          delete n characters from current position
 
@@ -48,8 +50,8 @@
              with control-N characters - if you don't use control-N you don't
              get an error - just weird results - if n <= 0 do it once
 
- nSstr/      search for str n times - position after match -
-             you can use python flavored regular expressions
+ nSstr/      search for str n times after current position - new position after
+             match - you can use python flavored regular expressions -
              if n <= 0 search once - there are no backwards searches
 
  nT          type n lines (see below)
@@ -306,9 +308,14 @@ def do_change(n, s, cp, p):
         cp = len(s)     # terminate this command string
         return cp, p
     if n <= 0:
-        neweditbuf = editbuf.replace(searchbuf, changebuf)
-    else:
-        neweditbuf = editbuf.replace(searchbuf, changebuf, n)
+        n = 0
+    try:
+        neweditbuf = re.sub(searchbuf, changebuf, editbuf[p:], n)
+    except Exception:
+        print('? regex')
+        cp = len(s)     # terminate this command string
+        return cp, p
+
     buf = []
     for ch in neweditbuf:
         buf.append(ch)
